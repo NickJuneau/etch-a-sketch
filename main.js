e03ce689a1a7771b8d2f6295035fc9eb0c3e17db
Nick Juneau <nickjuneau63@gmail.com>
2024-08-13 17:36:58 -0500

    Finished the button to create a new grid

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-const gridContainer = document.querySelector(".grid-container")
+//Creates a 16x16 grid once the webpage first loads
+let size = 16;
+const gridContainer = document.querySelector(".grid-container");
+createGrid(size);
 
-for(let i=0; i<16; i++){
-    const colBox = document.createElement("div");
-    colBox.classList.add("col-container");
-    gridContainer.appendChild(colBox)
-    for(let x=0; x<16; x++){
-        const box =  document.createElement("div");
-        box.classList.add("box")
-        colBox.appendChild(box);
+
+
+function createGrid(gridSize) {
+
+    for (let i = 0; i < gridSize; i++) {
+        const colBox = document.createElement("div");
+        colBox.classList.add("col-container");
+        document.querySelector(".grid-container").appendChild(colBox)
+        for (let x = 0; x < gridSize; x++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            colBox.appendChild(box);
+        }
+    }
+
+    let boxes = document.querySelectorAll(".box");
+
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mouseover", function () {
+            boxes[i].classList.add("covered");
+        });
     }
 }
 
-let boxes = document.querySelectorAll(".box");
 
-for(let i=0; i<boxes.length; i++){
-    boxes[i].addEventListener("mouseover", function() {
-        boxes[i].classList.add("covered");
-    });
-}
+
+
+
+
+const resetButton = document.querySelector("#reset-button");
+const content = document.querySelector(".content");
+
+resetButton.addEventListener("click", function () {
+    let tempSize = prompt("How many rows/columns should your new grid have?");
+    if (tempSize > 100 || tempSize < 0) {
+        alert("You must enter a value from 0-100 inclusive! Try Again.");
+    } else {
+        size = tempSize;
+        document.querySelector(".grid-container").remove();
+        const newDiv = document.createElement("div");
+        content.appendChild(newDiv);
+        newDiv.classList.add("grid-container");
+
+        createGrid(size);
+    }
+});
